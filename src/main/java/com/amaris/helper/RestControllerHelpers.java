eb57fc464407ead3f3bb2d9a312eb9a0e58d9aa6
keitaam <amadou.keita@ext.ec.europa.eu>
2025-09-05 06:20:14 +0200

    remove un used code in helpers

--- src/main/java/com/amaris/helper/RestControllerHelpers.java
+++ src/main/java/com/amaris/helper/RestControllerHelpers.java
@@ -22,35 +22,4 @@ public class RestControllerHelpers {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.convertValue(object, String.class);
     }
-
-
-    public static class RequestBody<T> {
-        int statusCode;
-        T body;
-        HttpExchange exchange;
-        Map<String, String> headers;
-
-        @Builder
-        public RequestBody(int code, T body, HttpExchange exchange, Map<String, String> headers) {
-            this.statusCode = code;
-            this.body = body;
-            this.exchange = exchange;
-            this.headers = headers;
-            try {
-                build();
-            } catch (IOException _) {
-
-            }
-        }
-
-        private void build() throws IOException {
-            String bodyJson = serialize(body);
-            exchange.sendResponseHeaders(statusCode, bodyJson.length());
-            headers.entrySet().stream().forEach(e -> exchange.getResponseHeaders().add(e.getKey(), e.getValue()));
-            OutputStream responseBody = exchange.getResponseBody();
-            responseBody.write(bodyJson.getBytes());
-            responseBody.close();
-        }
-    }
-
 }
