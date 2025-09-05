a4298a25395bb2dd98a3cabbd1600a9b376a81d0
keitaam <amadou.keita@ext.ec.europa.eu>
2025-09-05 06:29:28 +0200

    add annotation on controller

--- src/main/java/com/amaris/controller/POJOController.java
+++ src/main/java/com/amaris/controller/POJOController.java
@@ -1,16 +1,15 @@
 package com.amaris.controller;
 
+import com.amaris.annotation.RequestMapping;
 import com.amaris.entity.POJO;
 import com.amaris.helper.RequestBody;
 import com.amaris.helper.ResponseBody;
 import com.amaris.helper.RestControllerHelpers;
 import com.amaris.service.POJOService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -21,11 +20,9 @@ public class POJOController {
 
     private final POJOService pojoService;
 
-    public ResponseBody<List<POJO>> getPojo(HttpExchange exchange) throws IOException {
 
-        if (!RestControllerHelpers.requestMapping("/test", GET_METHOD, exchange)) {
-            return null;
-        }
+    @RequestMapping(path = "/test",method = GET_METHOD)
+    public ResponseBody<List<POJO>> getPojo(HttpExchange exchange) throws IOException {
         List<POJO> pojos = pojoService.getPojos();
         return ResponseBody.<List<POJO>>builder()
                 .body(pojos)
@@ -33,25 +30,21 @@ public class POJOController {
                 .build();
     }
 
+    @RequestMapping(path = "/test",method = POST_METHOD)
     public POJO createPOJO(HttpExchange exchange) throws IOException {
-        if (!RestControllerHelpers.requestMapping("/test", POST_METHOD, exchange)) {
-            return null;
-        }
         RequestBody<POJO> pojo = RequestBody.<POJO>builder() // equivalent @RequestBody Pojo pojo
                 .exchange(exchange)
                 .clazz(POJO.class)
                 .build();
 
-        RequestBody<POJO> pojo = RequestBody.<POJO>builder().exchange(exchange).build();
+        POJO pojoObject=pojoService.createPojo(pojo.getBody());
 
-        POJO pojoObject=pojo.getBody();
         ResponseBody.<POJO>builder()
                 .body(pojoObject)
                 .exchange(exchange)
                 .headers(Map.of("Content-Type", "application/json", "Accept", "*"))
                 .build();
-
-        return pojoService.createPojo(pojoObject);
+        return pojoObject;
     }
 
 
