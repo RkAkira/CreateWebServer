0ea941178cdb4f39788ce3b2411d5643faf0245a
keitaam <amadou.keita@ext.ec.europa.eu>
2025-09-05 06:34:27 +0200

    refactoring annotation processor

--- src/main/java/com/amaris/annotation/AnnotationProcessor.java
+++ src/main/java/com/amaris/annotation/AnnotationProcessor.java
@@ -16,19 +16,25 @@ public class AnnotationProcessor {
     public void handleAnnotation(HttpExchange exchange) {
         Method[] methods = controller.getClass().getMethods();
         for (Method method : methods) {
-            if (method.isAnnotationPresent(RequestMapping.class)) {
-                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
-                if (RestControllerHelpers.requestMapping(annotation.path(), annotation.method(), exchange)) {
-                    method.setAccessible(true);
-                    try {
-                        method.invoke(controller, exchange); // equivalent a faire controller.method(exchange)
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException(e);
-                    }
-                }
-
-            }
+            handleRequestMapping(exchange, method);
         }
     }
+
+    private void handleRequestMapping(HttpExchange exchange, Method method) {
+        if (!method.isAnnotationPresent(RequestMapping.class)) {
+            return;
+        }
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        if (!RestControllerHelpers.requestMapping(annotation.path(), annotation.method(), exchange)) {
+            return;
+        }
+        method.setAccessible(true);
+        try {
+            method.invoke(controller, exchange); // equivalent a faire controller.method(exchange)
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
 }
